7a979959d6cbf47944e7a2b8422c66fff2eb7f4c
Tarik Havighorst <nomail@tarik.invalid>
2021-11-17 15:36:26 +0100

    added systray config

--- config.h
+++ config.h
@@ -3,6 +3,15 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected
+                                                   monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft  = 1;  	/* 0: systray in the right corner, >0:
+                                                   systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray
+                                                   on the first monitor, False: display
+                                                   systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -28,6 +37,7 @@ static const Rule rules[] = {
 	 */
 	/* class        instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",       NULL,       NULL,       0,            1,           -1 },
+        { "QjackCtl",   NULL,       NULL,       0,            1,           -1 },
 	{ "firefox",    NULL,       NULL,       1 << 8,       0,           -1 },
 	{ NULL,         "Mail",     NULL,       1 << 7,       0,           -1 },
 };
